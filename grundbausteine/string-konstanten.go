c71b0ed7127c3dd792fd0e6b76dcf3de09eb2045
Bertram Scharpf <software@bertram-scharpf.de>
2023-02-06 12:29:04 +0100

    Mehr

--- grundbausteine/string-konstanten.go
+++ grundbausteine/string-konstanten.go
@@ -15,4 +15,6 @@ func main() {
 	showit("Es kostet \U000020ac 100,-")
 	showit("Es kostet \xe2\x82\xac 100,-")
 	showit("Es kostet \xe2\x82 100,-") // ungültiges UTF-8
+	showit("\xf0\x9f\x92\xa9")
+	showit("\U0001f4a9")
 }
