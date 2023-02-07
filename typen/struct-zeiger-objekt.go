ae1e11db6475a70e537e1a090248dfd18000934c
Bertram Scharpf <software@bertram-scharpf.de>
2023-02-07 11:21:43 +0100

    Mehr

---
+++ typen/struct-zeiger-objekt.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+type Mark struct {
+	V     *Vertex
+	Title string
+}
+
+func dontsetit(m Mark) {
+	m.V.X, m.V.Y = 33, 44 // Setzt es trotzdem!
+	fmt.Printf("%v %T\n", &m.Title, &m.Title)
+	m.Title = "bar"
+	fmt.Printf("%v %T\n", &m.Title, &m.Title)
+}
+
+func setit(m *Mark) {
+	m.V.X, m.V.Y = 55, 66
+	fmt.Printf("%v %T\n", &m.Title, &m.Title)
+	m.Title = "baz"
+	fmt.Printf("%v %T\n", &m.Title, &m.Title)
+}
+
+func main() {
+	var m = Mark{V: &Vertex{1, 2}, Title: "foo"}
+
+	fmt.Println(m, m.V)
+	dontsetit(m)
+	fmt.Println(m, m.V)
+	setit(&m)
+	fmt.Println(m, m.V)
+}
