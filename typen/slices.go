5060215d6ddd0f0f0cf83c85ff44aa4fb66154f7
Bertram Scharpf <software@bertram-scharpf.de>
2023-02-07 12:00:02 +0100

    Mehr

--- typen/slices.go
+++ typen/slices.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	primes := [...]int{2, 3, 5, 7, 11, 13}
+	// primes:         0__1__2__3__4___5_
+	// s:					 0__1__2_
 	fmt.Printf("%v (%T)\n", primes, primes)
 
-	var s []int = primes[1:4]
+	var s []int = primes[2:5]
 	fmt.Printf("%v (%T)\n", s, s)
 	fmt.Println(s)
+
+	primes[1] = 37
+	fmt.Printf("%v (%T)\n", primes, primes)
+	fmt.Printf("%v (%T)\n", s, s)
+
+	s[2] = 73
+	fmt.Printf("%v (%T)\n", primes, primes)
+	fmt.Printf("%v (%T)\n", s, s)
 }
